bf410d2baa6be3d630462f040dc67a49b66f610c
nk-akun <nk_akun@163.com>
2020-04-06 22:29:31 +0800

    Add test_sds

--- engine/test_sds.go
+++ engine/test_sds.go
@@ -3,12 +3,33 @@ package godis
 import "fmt"
 
 // TestSds ...
-func TestSds() {
+func Test_Sds() {
 
-	str := "abcdef"
+	str := "abcdefse"
 	s1 := SdsNew(&str)
-	str = "qwertt"
+	str = "abcdefs"
 	s2 := SdsNew(&str)
 
+	//cmp
+	fmt.Println(SdsCmp(s1, s2))
+
+	// len
 	fmt.Println(s1.SdsLen(), s2.SdsLen())
+
+	//get string
+	fmt.Println(*s1.SdsGetString())
+
+	//copy
+	str = "poiuytersds"
+	s1.SdsCopy(&str)
+
+	fmt.Println(s1.SdsLen(), *s1.SdsGetString())
+
+	//cat
+	str = "123213"
+	s1.SdsCat(&str)
+	fmt.Println(s1.SdsLen(), *s1.SdsGetString())
+
+	s1.SdsCatSds(s2)
+	fmt.Println(s1.SdsLen(), *s1.SdsGetString())
 }
