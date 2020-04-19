7e9ec13cbfae533de0088942e48c3c8eccfed12a
nk-akun <nk_akun@163.com>
2020-04-19 22:16:16 +0800

    Add delete and update functions in zset

--- engine/zset.go
+++ engine/zset.go
@@ -61,7 +61,7 @@ func zslRandomLevel() int {
 }
 
 // Insert insert node into skiplist
-func (zsl *ZskipList) Insert(score float64, value *Sdshdr) {
+func (zsl *ZskipList) Insert(score float64, value *Sdshdr) *ZskipListNode {
 	// borders[i] means we need insert node after borders[i] when level is i
 	// dis[i] is distence from head to borders[i]
 	borders := make([]*ZskipListNode, ZSL_MAX_LEVEL)
@@ -110,4 +110,71 @@ func (zsl *ZskipList) Insert(score float64, value *Sdshdr) {
 	} else {
 		node.level[0].forward.backward = node
 	}
+	zsl.length++
+	return node
+}
+
+// Delete remove the node with score and value
+func (zsl *ZskipList) Delete(score float64, value *Sdshdr) {
+	borders := make([]*ZskipListNode, ZSL_MAX_LEVEL)
+	p := zsl.header
+
+	for i := zsl.level; i >= 0; i-- {
+		for p.level[i].forward != nil && (p.level[i].forward.score < score || (p.level[i].forward.score == score && SdsCmp(p.level[i].forward.value, value) == -1)) {
+			p = p.level[i].forward
+		}
+		borders[i] = p
+	}
+
+	node := p.level[0].forward
+	if node != nil && node.score == score && SdsCmp(node.value, value) == 0 {
+		zsl.DeleteNode(node, borders)
+	}
+}
+
+// DeleteNode remove node, connect the forward node and backward node
+func (zsl *ZskipList) DeleteNode(node *ZskipListNode, borders []*ZskipListNode) {
+	// update forward
+	for i := 0; i < zsl.level; i++ {
+		if borders[i].level[i].forward == node {
+			borders[i].level[i].forward = node.level[i].forward
+			borders[i].level[i].span += node.level[i].span - 1
+		} else {
+			borders[i].level[i].span--
+		}
+	}
+
+	// update backward
+	if node.level[0].forward != nil {
+		node.level[0].forward.backward = node.backward
+	} else {
+		zsl.tail = node.backward
+	}
+	zsl.length--
+}
+
+// Update search the real node with curScore and newScore,update its score or delete it and recreate a new node
+func (zsl *ZskipList) Update(value *Sdshdr, curScore float64, newScore float64) *ZskipListNode {
+	borders := make([]*ZskipListNode, ZSL_MAX_LEVEL)
+	p := zsl.header
+
+	for i := zsl.level; i >= 0; i-- {
+		for p.level[i].forward != nil && (p.level[i].forward.score < curScore || (p.level[i].forward.score == curScore && SdsCmp(p.level[i].forward.value, value) == -1)) {
+			p = p.level[i].forward
+		}
+		borders[i] = p
+	}
+
+	node := p.level[0].forward
+
+	// if the forward is larger or NULL and backward is smaller or NULL
+	if (node.level[0].forward == nil || node.level[0].forward.score > newScore) && (node.backward == nil || node.backward.score < newScore) {
+		node.score = newScore
+		return node
+	}
+
+	// it's not sure of the node's position,delete it and recreate
+	zsl.DeleteNode(node, borders)
+	newNode := zsl.Insert(newScore, node.value)
+	return newNode
 }
